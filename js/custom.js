e5c27fb0df6441ebcc48fcb7dae74e4c2defb794
darius <dariusa.bualanjr@pccw.com>
2022-05-25 12:19:36 +0800

    Deploy website - based on 6d2aef159872f0f68d42a45b9fabe91d58e25df1

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,14 @@
 window.onload = function() {
-  document.querySelectorAll('.menu__list-item.menu__list-item--collapsed > a').forEach(item => {
-    item.click();
-  });
+  // document.querySelectorAll('.menu__list-item.menu__list-item--collapsed > a').forEach(item => {
+  //   item.click();
+  // });
+
+  /**
+   * Google analytics
+   */
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-229775239-1');
+
 }
